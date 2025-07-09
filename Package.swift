5e5ca0321ab11c65950e96431f9a1c1211b8bea7
Jed Gyeong <jed@sendbird.com>
2025-07-09 14:01:54 +0900

    Updated minimum iOS version to iOS 14 Resolved an issue where live events with a cover image were not being created properly

--- Package.swift
+++ Package.swift
@@ -3,28 +3,30 @@
 import PackageDescription
 let package = Package(
     name: "SendbirdLiveSDK",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v14)],
     products: [
         .library(
             name: "SendbirdLiveSDK",
             targets: ["SendbirdLiveSDKTarget"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/sendbird/sendbird-webrtc-ios",  "1.8.1"..<"1.9.0"),
-        .package(url: "https://github.com/sendbird/sendbird-chat-sdk-ios", from: "4.21.3")
+        .package(url: "https://github.com/sendbird/sendbird-webrtc-ios",  "1.9.0"..<"1.10.0"),
+        .package(url: "https://github.com/sendbird/sendbird-chat-sdk-ios", from: "4.27.2")
     ],
     targets: [
         .binaryTarget(
             name: "SendbirdLiveSDK",
-            url: "https://github.com/sendbird/sendbird-live-sdk-ios/releases/download/v1.3.0/SendbirdLiveSDK.xcframework.zip",
-            checksum: "dd1c968324790e11dc923d9fa3ffdd16ed5bbd5cac6d5df982ad08d80899f68e"
+            url: "https://github.com/sendbird/sendbird-live-sdk-ios/releases/download/v1.4.0/SendbirdLiveSDK.xcframework.zip",
+            checksum: "a3bcf083e46e84069ec28d92a6aa576b74a6b2ae87bae2df8c2bb009a7b4216b"
         ),
-        .target(name: "SendbirdLiveSDKTarget",
-                dependencies: [
-                    .target(name: "SendbirdLiveSDK"),
-                    .product(name: "WebRTC", package: "sendbird-webrtc-ios"),
-                    .product(name: "SendbirdChatSDK", package: "sendbird-chat-sdk-ios")
-                ],
-                path: "Sources"),
+        .target(
+            name: "SendbirdLiveSDKTarget",
+            dependencies: [
+                .target(name: "SendbirdLiveSDK"),
+                .product(name: "WebRTC", package: "sendbird-webrtc-ios"),
+                .product(name: "SendbirdChatSDK", package: "sendbird-chat-sdk-ios")
+            ],
+            path: "Sources"
+        )
     ]
 )
